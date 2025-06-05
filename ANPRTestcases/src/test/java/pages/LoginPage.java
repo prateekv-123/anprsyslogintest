9f4e8de2ae296ade64df3ba6e3c02a42c32ff189
PrateekProjects <prateekvarshney1106@gmail.com>
2025-06-05 13:21:47 +0530

    Both the files are updated successfully.

--- ANPRTestcases/src/test/java/pages/LoginPage.java
+++ ANPRTestcases/src/test/java/pages/LoginPage.java
@@ -2,47 +2,67 @@ package pages;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
 
 public class LoginPage {
     WebDriver driver;
+    WebDriverWait wait;
 
-    @FindBy(id = "username") WebElement usernameField;
-    @FindBy(id = "password") WebElement passwordField;
-    @FindBy(id = "loginBtn") WebElement loginButton;
-    @FindBy(id = "eyeIcon") WebElement eyeIcon;
-    @FindBy(id = "errorMsg") WebElement errorMessage; // Add locator if error message shows
+    @FindBy(id = "username")
+    WebElement usernameField;
+
+    @FindBy(id = "password")
+    WebElement passwordField;
+
+    @FindBy(id = "loginBtn")
+    WebElement loginButton;
+
+    @FindBy(id = "eyeIcon")
+    WebElement eyeIcon;
+
+    @FindBy(id = "errorMsg")
+    WebElement errorMessage;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public void enterUsername(String username) {
+        wait.until(ExpectedConditions.visibilityOf(usernameField));
         usernameField.clear();
         usernameField.sendKeys(username);
     }
 
     public void enterPassword(String password) {
+        wait.until(ExpectedConditions.visibilityOf(passwordField));
         passwordField.clear();
         passwordField.sendKeys(password);
     }
 
     public void clickLogin() {
+        wait.until(ExpectedConditions.elementToBeClickable(loginButton));
         loginButton.click();
     }
 
     public String getPasswordFieldType() {
+        wait.until(ExpectedConditions.visibilityOf(passwordField));
         return passwordField.getAttribute("type");
     }
 
     public void togglePasswordVisibility() {
+        wait.until(ExpectedConditions.elementToBeClickable(eyeIcon));
         eyeIcon.click();
     }
 
     public boolean isErrorMessageDisplayed() {
         try {
+            wait.until(ExpectedConditions.visibilityOf(errorMessage));
             return errorMessage.isDisplayed();
-        } catch (NoSuchElementException e) {
+        } catch (TimeoutException | NoSuchElementException e) {
             return false;
         }
     }
--- ANPRTestcases/src/test/java/utils/BaseTest.java
+++ ANPRTestcases/src/test/java/utils/BaseTest.java
@@ -2,6 +2,7 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -15,7 +16,16 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        // Headless mode for Jenkins or CI environments
+        options.addArguments("--headless=new");  // "--headless" for older versions
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--no-sandbox");  // for Linux environments, safe to keep
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
